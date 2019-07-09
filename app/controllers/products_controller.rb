057f3592de9ff82e070fbf302369071702fc84f6
elizyon <eliza.h.y@tumo.org>
2019-07-09 18:57:17 +0400

    made the basics of the site

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,47 @@
+class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
+
+	def show 
+		@product = Product.find(params[:id])
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		 @product = Product.find(params[:id])
+		 @product.update(product_params)
+
+		 redirect_to product_path(@product)
+
+	end
+
+	def create
+		product = Product.create(product_params)
+
+		redirect_to product_path(product)
+	end
+
+	def destroy
+		@product = Product.find(params[:id])
+
+		@product.destroy
+
+		redirect_to products_path
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name , :description, :photo, :price )	
+	end
+
+
+end
